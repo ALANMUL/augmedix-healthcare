4b2b5fb9d50fdc68bba0dd75a4e078fca25eede8
AL Anamul Mustakim <70197435+ALANMUL@users.noreply.github.com>
2026-09-10 23:37:43 +0600

    Reformat patients_raw.sql table definition

--- sql/raw/patients_raw.sql
+++ sql/raw/patients_raw.sql
@@ -6,27 +6,27 @@
 
 CREATE TABLE IF NOT EXISTS `augmedix-healthcare.augmedix.raw_patients` (
   patient_id                     STRING,
-  mrn                              STRING,
-  first_name                        STRING,
-  last_name                            STRING,
-  date_of_birth                          STRING,   -- cast in staging
-  age                                       INT64,
-  gender                                       STRING,
-  race                                            STRING,
-  ethnicity                                          STRING,
-  preferred_language                                    STRING,
-  address_line1                                            STRING,
-  city                                                        STRING,
-  state                                                          STRING,
-  zip_code                                                          INT64,
-  primary_insurance_member_id                                          STRING,
-  pcp_id                                                                   STRING,
-  deceased                                                                    STRING,
-  created_date                                                                   STRING,   -- cast in staging
-  smoking_status                                                                    STRING,
-  alcohol_use                                                                           STRING,
-  primary_insurance_name_id                                                                INT64,
-  secondary_insurance_name_id                                                                  FLOAT64  -- has nulls
+  mrn                            STRING,
+  first_name                     STRING,
+  last_name                      STRING,
+  date_of_birth                  STRING,   -- cast in staging
+  age                            INT64,
+  gender                         STRING,
+  race                           STRING,
+  ethnicity                      STRING,
+  preferred_language             STRING,
+  address_line1                  STRING,
+  city                           STRING,
+  state                          STRING,
+  zip_code                       INT64,
+  primary_insurance_member_id    STRING,
+  pcp_id                         STRING,
+  deceased                       STRING,
+  created_date                   STRING,   -- cast in staging
+  smoking_status                 STRING,
+  alcohol_use                    STRING,
+  primary_insurance_name_id      INT64,
+  secondary_insurance_name_id    FLOAT64  -- has nulls
 );
 
 -- Load pattern (bq CLI):
