90116a71947095401e399b2c2155cef7978830a6
AL Anamul Mustakim <70197435+ALANMUL@users.noreply.github.com>
2026-09-10 23:29:41 +0600

    Reformat CREATE TABLE statement for lab tests

--- sql/raw/dim_lab_test_raw.sql
+++ sql/raw/dim_lab_test_raw.sql
@@ -8,16 +8,16 @@
 
 CREATE TABLE IF NOT EXISTS `augmedix-healthcare.augmedix.raw_dim_lab_test` (
   lab_test_id            STRING,
-  lab_test_name             STRING,
-  lab_abbreviation             STRING,
-  lab_category                    STRING,
-  body_system                        STRING,
-  is_fasting_required                   STRING,
-  critical_high                            STRING,
-  critical_low                                STRING,
-  unit                                           STRING,
-  loinc_code                                        STRING,
-  description                                          STRING
+  lab_test_name          STRING,
+  lab_abbreviation       STRING,
+  lab_category           STRING,
+  body_system            STRING,
+  is_fasting_required    STRING,
+  critical_high          STRING,
+  critical_low           STRING,
+  unit                   STRING,
+  loinc_code             STRING,
+  description            STRING
 );
 
 -- Load pattern (bq CLI):
